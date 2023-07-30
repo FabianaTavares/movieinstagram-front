4e2145848f0c90f59d93f9f9a605bd80d7a79769
Fabiana Tavares <fabi.tavares1@gmail.com>
2023-07-30 18:58:03 -0300

    feat: teste

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-prettier/recommended', 'stylelint-config-prettier'],
+  extends: ['stylelint-prettier/recommended', 'stylelint-config-standard'],
   plugins: ['stylelint-prettier'],
   rules: {
     'prettier/prettier': true
